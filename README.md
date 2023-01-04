2e1c7a383871f91c6bc149a4aae6f1689fa51951
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2023-01-03 22:11:41 -0300

    migrate to warp web framework
    Signed-off-by: Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Backend but com rusbé.
 
-> Não tem muita coisa pra ver por aqui ainda, mas se você quiser dar uma olhada no código, fique a vontade.
+> Não tem muita coisa pra ver por aqui ainda, mas se você quiser dar uma olhada no código,
+> fique a vontade.
--- cerberus/src/main.rs
+++ cerberus/src/main.rs
@@ -1,28 +1,25 @@
-use actix_web::{App, get, HttpResponse, HttpServer, Responder};
-use actix_web::http::StatusCode;
-use serde::Serialize;
+use std::collections::HashMap;
+use warp::Filter;
 
+async fn hello_fn(name: String) ->Result< impl warp::Reply, warp::Rejection> {
+    let mut response: HashMap<String,String> = HashMap::new();
 
+    response.insert(String::from("Hello"), String::from(name));
 
-#[get("/greet")]
-async fn greet() -> impl Responder {
-    #[derive(Serialize)]
-    struct Response {
-        hello: String,
-    }
-
-    let res = Response{ hello: "world".to_string() };
-
-    HttpResponse::build(StatusCode::CREATED).json(res)
+    Ok(warp::reply::json(&response))
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(greet)
-    })
-        .bind(("0.0.0.0", 3000))?
-        .run()
-        .await
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::get()
+        .and(warp::path("hello"))
+        .and(warp::path::param())
+        .and(warp::path::end())
+        .and_then(hello_fn);
+
+    warp::serve(hello)
+        .run(([0, 0, 0, 0], 3030))
+        .await;
+
 }
