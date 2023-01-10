1216c33d180e32c8ee0132a819c76afb87955762
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2023-01-10 17:10:38 -0300

    Project Setup (#2)
    Close #1 
    
    First Setup with basic stuff to start the project
    
    Signed-off-by: Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>

--- cerberus/src/main.rs
+++ cerberus/src/main.rs
@@ -1,28 +1,6 @@
-use actix_web::{App, get, HttpResponse, HttpServer, Responder};
-use actix_web::http::StatusCode;
-use serde::Serialize;
+use cerberus::*;
 
-
-
-#[get("/greet")]
-async fn greet() -> impl Responder {
-    #[derive(Serialize)]
-    struct Response {
-        hello: String,
-    }
-
-    let res = Response{ hello: "world".to_string() };
-
-    HttpResponse::build(StatusCode::CREATED).json(res)
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(greet)
-    })
-        .bind(("0.0.0.0", 3000))?
-        .run()
-        .await
+#[tokio::main]
+async fn main() {
+    start().await;
 }
