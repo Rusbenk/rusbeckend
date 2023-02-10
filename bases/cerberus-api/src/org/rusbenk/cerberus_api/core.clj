420f89e6fe9b8adaef37e9cea233e726f0d9821d
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2023-02-10 20:40:26 -0300

    wip: cerberus initial structure

---
+++ bases/cerberus-api/src/org/rusbenk/cerberus_api/core.clj
@@ -0,0 +1,12 @@
+(ns org.rusbenk.cerberus-api.core
+  (:gen-class))
+
+;; Namespace de rotas
+;; Namespace de Handlers
+;; Namespace de config
+;; - Setup do server
+;; - Setup dos interceptors default
+;; - Setup de parsing/coercion
+
+(defn -main [& _]
+  (println "Hello World"))
---
+++ bases/cerberus-api/test/org/rusbenk/cerberus_api/core_test.clj
@@ -0,0 +1,6 @@
+(ns org.rusbenk.cerberus-api.core-test
+  (:require [clojure.test :as test :refer :all]
+            [org.rusbenk.cerberus-api.core :as core]))
+
+(deftest dummy-test
+  (is (= 1 1)))
