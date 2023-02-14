c69a36559304fe149feced4433756acfb7e0fac0
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2023-02-13 23:27:21 -0300

    initial server setup
    Signed-off-by: Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>

--- bases/cerberus-api/src/org/rusbenk/cerberus_api/core.clj
+++ bases/cerberus-api/src/org/rusbenk/cerberus_api/core.clj
@@ -1,12 +1,73 @@
 (ns org.rusbenk.cerberus-api.core
-  (:gen-class))
+  (:gen-class)
+  (:require [reitit.ring :as r.ring]
+            [ring.adapter.jetty :as ring.jetty]
+            [reitit.ring.middleware.parameters :as r.ring.parameters])
+  (:import (org.eclipse.jetty.server Server)))
+
+(defonce ^:private server (atom nil))
+
+(defn gen-routes []
+  [["/api"
+    ["/health" {:get {:handler (constantly
+                                 {:status  200
+                                  :headers {"Content-Type" "application/edn"}
+                                  :body    "{:state :healthy}"})}}]]])
+
+(defn gen-router [routes]
+  (r.ring/router
+    routes
+    {:data {
+            ;; Data coercion with Malli
+            ;:coercion reitit.coercion.spec/coercion
+
+            ;; Content Negotiation
+            ;:muuntaja m/instance
+
+            ;; Middlewares
+            :middleware [r.ring.parameters/parameters-middleware]}}))
+
+(defn gen-app []
+  (r.ring/ring-handler
+    (gen-router (gen-routes))))
+
+(defn start! [opts]
+  (when-not (nil? @server)
+    (throw (ex-info "Server was already started" nil)))
+  (reset! server
+          (ring.jetty/run-jetty
+            (gen-app)
+            opts)))
+
+(defn stop! []
+  (when (nil? @server)
+    (throw (ex-info "Server is already stopped" nil)))
+  (let [^Server s @server]
+    (.stop s)
+    (reset! server nil)))
 
 ;; Namespace de rotas
 ;; Namespace de Handlers
 ;; Namespace de config
-;; - Setup do server
-;; - Setup dos interceptors default
-;; - Setup de parsing/coercion
+;; - Setup: Content Negotiation (req+res JSON)
+;; - Setup: Coercion + Validation (malli)
+;; - Setup: Swagger
 
 (defn -main [& _]
-  (println "Hello World"))
+  (println "TODO"))
+
+
+(comment
+
+
+  (do
+
+    (stop!)
+
+    (start! {:join? false
+             :port  3000
+             :host  "0.0.0.0"})
+
+    )
+
+  )
